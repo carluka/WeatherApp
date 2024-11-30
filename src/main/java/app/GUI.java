7a9937af732ea67df7d87bec82411fb8a1449c53
carluka <carluka33@gmail.com>
2024-11-30 15:12:54 +0100

    Logika za pridobivanje vremenske napovedi

--- src/main/java/app/GUI.java
+++ src/main/java/app/GUI.java
@@ -2,32 +2,49 @@ package app;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.util.List;
 
 public class GUI extends JFrame{
     private JTextField vnosnoPolje;
     private JButton gumbIskanje;
+    private JPanel inputPanel;
+    private JPanel grafPanel;
 
     public GUI(){
         setTitle("Weather app");
-        setSize(600,500);
+        setSize(800,600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         vnosnoPolje = new JTextField(30);
         gumbIskanje = new JButton("Prikaži vreme");
 
-        JPanel inputPanel = new JPanel();
+        inputPanel = new JPanel();
         inputPanel.add(new JLabel("Vnesite lokacijo:"));
         inputPanel.add(vnosnoPolje);
         inputPanel.add(gumbIskanje);
 
+        grafPanel = new JPanel();
+
         setLayout(new BorderLayout());
         add(inputPanel, BorderLayout.NORTH);
-        gumbIskanje.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                MeteoAPI.pridobiVremenskoNapoved(vnosnoPolje.getText());
+        add(grafPanel, BorderLayout.CENTER);
+
+        gumbIskanje.addActionListener(e -> pridobiInPrikaziVreme());
+    }
+
+    private void pridobiInPrikaziVreme(){
+        String lokacija = vnosnoPolje.getText().trim();
+
+        if(!lokacija.isEmpty()){
+            List<List<?>> napoved = MeteoAPI.pridobiVremenskoNapoved(lokacija);
+            if(napoved != null){
+                List<String> time = (List<String>) napoved.get(0);
+                List<Double> temperature = (List<Double>) napoved.get(1);
+            } else {
+                JOptionPane.showMessageDialog(this, "Za to lokacijo ni mogoče pridobiti vremenske napovedi.");
             }
-        });
+        } else {
+            JOptionPane.showMessageDialog(this, "Prosim vnesite lokacijo.");
+        }
     }
 }
--- src/main/java/app/MeteoAPI.java
+++ src/main/java/app/MeteoAPI.java
@@ -5,6 +5,8 @@ import org.json.JSONObject;
 
 import java.net.URI;
 import java.net.http.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MeteoAPI {
     public static final String URL = "https://api.open-meteo.com/v1/forecast";
@@ -36,12 +38,45 @@ public class MeteoAPI {
             return null;
         }
     }
-    public static void pridobiVremenskoNapoved(String mesto){
+
+
+
+    public static List<List<?>> pridobiVremenskoNapoved(String mesto){
         String[] koordinate = pridobiKoordinate(mesto);
         if(koordinate != null){
-            //PRIDOBI VREMENSKE PODATKE
+            try {
+                String url = URL + "?latitude=" + koordinate[0] + "&longitude=" + koordinate[1] + "&current=temperature_2m,weather_code&hourly=temperature_2m";
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(url))
+                        .GET()
+                        .build();
+
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                JSONObject jsonResponse = new JSONObject(response.body());
+
+                JSONObject hourly = jsonResponse.getJSONObject("hourly");
+                JSONObject current = jsonResponse.getJSONObject("current");
+
+                JSONArray timeResponse = hourly.getJSONArray("time");
+                JSONArray temperatureResponse = hourly.getJSONArray("temperature_2m");
+
+                List<String> time = new ArrayList<>();
+                List<Double> temperature = new ArrayList<>();
+                for(int i = 0; i < timeResponse.length(); i++){
+                    time.add(timeResponse.getString(i));
+                    temperature.add(temperatureResponse.getDouble(i));
+                }
+
+                List<List<?>> skupaj = new ArrayList<>();
+                skupaj.add(time);
+                skupaj.add(temperature);
+                return skupaj;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
-
+        return null;
     }
 }
